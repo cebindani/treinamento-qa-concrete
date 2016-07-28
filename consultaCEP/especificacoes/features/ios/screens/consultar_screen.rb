32f696935fe4c5dd090f46237d08a83d7c269296
dmaila <dmaila@ciandt.com>
2016-07-28 10:55:11 -0300

    Adicionado mapeamento dos elementos de tela do iOS

--- consultaCEP/especificacoes/features/ios/screens/consultar_screen.rb
+++ consultaCEP/especificacoes/features/ios/screens/consultar_screen.rb
@@ -1,5 +1,22 @@
 # -*- encoding : utf-8 -*-
 class ConsultarScreen < IOSScreenBase
+
+  trait(:trait)       { "* marked:'#{layout_name}'" } #marked é qquer elemento
+
+
+  element(:layout_name)           { 'CONSULTAR_SCREEN' }
+  element(:campo_cep)             { 'CAMPO_CEP' }
+  element(:botao_consultar)       { 'Buscar' }
+
+
+  action(:apertar_botao_consultar) {
+    touch_screen_element (botao_consultar)
+  }
+
+  def digitar_cep (cep)
+    enter(cep, campo_cep)
+  end
+
   # Identificador da tela
   # trait(:trait)                 { "* marked:'#{layout_name}'" }
 
